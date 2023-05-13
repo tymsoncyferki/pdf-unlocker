80a8d038b5e10a10511505a5f37701f94678f896
tymsoncyferki <tymekurban@gmail.com>
2023-05-13 14:14:30 +0200

    configuration file

---
+++ config.py
@@ -0,0 +1,2 @@
+pdf_passwords = ["statystyka_nas_przenika", "urocze_zadanka", "naukaR=kariera"]
+absolute_path = ""
--- unlock.py
+++ unlock.py
@@ -1,25 +1,41 @@
 import pikepdf
-import os
 import glob
+import os
+from config import pdf_passwords, absolute_path
+import argparse
 
 
 def unlock():
-    cwd = os.getcwd()
-    pdf_pass = 'statystyka_nas_przenika'
-    pdf_pass2 = 'urocze_zadanka'
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--current", help="Use current directory instead of absolute path", action="store_true")
+    args = parser.parse_args()
+    if args.current or not absolute_path:
+        print("Using current directory")
+        cwd = os.getcwd()
+    else:
+        print("Using absolute path")
+        cwd = absolute_path
     for file in glob.glob(cwd + '\\*.pdf'):
         print(file)
+        unlocked = False
         try:
-            pdf = pikepdf.open(file, password=pdf_pass, allow_overwriting_input=True)
+            pdf = pikepdf.open(file)
+            print("PDF do not have a password")
+            continue
         except (PermissionError, Exception):
+            pass
+        for password in pdf_passwords:
             try:
-                pdf = pikepdf.open(file, password=pdf_pass2, allow_overwriting_input=True)
+                pdf = pikepdf.open(file, password=password, allow_overwriting_input=True)
+                unlocked = True
+                break
             except (PermissionError, Exception):
-                print('cannot unlock pdf')
                 continue
-        print('Processing')
+        if not unlocked:
+            print('Cannot unlock pdf')
+            continue
         pdf.save(file)
-        print('Files unlocked succesfully')
+        print('File unlocked succesfully')
 
 
 if __name__ == '__main__':
