6d96ac61431cd1937e4729a78d26d5e3b36ee988
Tymoteusz Urban <115067539+tymsoncyferki@users.noreply.github.com>
2023-06-08 14:08:46 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-pdf_passwords = ["statystyka_nas_przenika", "urocze_zadanka", "naukaR=kariera"]
+pdf_passwords = ["statystyka_nas_przenika", "urocze_zadanka", "naukaR=kariera", "wzory", "tablice"]
 absolute_path = ""
