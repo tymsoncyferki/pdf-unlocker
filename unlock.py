021f43bdc781a845e5045b542da6962c051e2d65
tymsoncyferki <tymekurban@gmail.com>
2023-06-13 20:02:52 +0200

    extraction

--- unlock.py
+++ unlock.py
@@ -5,39 +5,64 @@ from config import pdf_passwords, absolute_path
 import argparse
 
 
-def unlock():
+def get_directory(arg, path):
+    """ Gets working directory """
+    if arg or not path:
+        print("Using current directory")
+        return os.getcwd()
+    else:
+        print("Using absolute path")
+        return absolute_path
+
+
+def is_encrypted(file):
+    """ Checks if pdf is encrypted """
+    try:
+        pdf = pikepdf.open(file)
+        print("PDF does not have a password")
+        return False
+    except (PermissionError, pikepdf.PasswordError):
+        return True
+
+
+def unlock(file, passwords):
+    """ Tries unlocking pdf with given list of passwords """
+    unlocked = False
+    for password in passwords:
+        try:
+            pdf = pikepdf.open(file, password=password, allow_overwriting_input=True)
+            unlocked = True
+            return unlocked, pdf
+        except (PermissionError, pikepdf.PasswordError):
+            continue
+    return unlocked, None
+
+
+def main():
+    # Get argument
     parser = argparse.ArgumentParser()
     parser.add_argument("-c", "--current", help="Use current directory instead of absolute path", action="store_true")
     args = parser.parse_args()
-    if args.current or not absolute_path:
-        print("Using current directory")
-        cwd = os.getcwd()
-    else:
-        print("Using absolute path")
-        cwd = absolute_path
+
+    # Get directory
+    cwd = get_directory(args.current, absolute_path)
+
+    # Iterate over pdf files
     for file in glob.glob(cwd + '\\*.pdf'):
         print(file)
-        unlocked = False
-        try:
-            pdf = pikepdf.open(file)
-            print("PDF do not have a password")
+        if not is_encrypted(file):
             continue
-        except (PermissionError, Exception):
-            pass
-        for password in pdf_passwords:
-            try:
-                pdf = pikepdf.open(file, password=password, allow_overwriting_input=True)
-                unlocked = True
-                break
-            except (PermissionError, Exception):
-                continue
+
+        unlocked, pdf = unlock(file, pdf_passwords)
+
         if not unlocked:
             print('Cannot unlock pdf')
             continue
+
         pdf.save(file)
         print('File unlocked succesfully')
 
 
 if __name__ == '__main__':
-    unlock()
+    main()
 
